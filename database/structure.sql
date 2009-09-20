42a893f9b8a612afd5a4385bd87409bd62feef26
darioteixeira <darioteixeira@yahoo.com>
2009-09-20 15:31:43 +0000

    Yet more tweaks to database organisation

--- database/structure.sql
+++ database/structure.sql
@@ -2,6 +2,16 @@
 /* Creates the basic structure of a Lambdium database.			*/
 /************************************************************************/
 
+/************************************************************************/
+/* Type of the various identifiers.					*/
+/************************************************************************/
+
+CREATE DOMAIN id_t              AS int4;
+CREATE DOMAIN timezone_id_t     AS id_t;
+CREATE DOMAIN user_id_t         AS id_t;
+CREATE DOMAIN story_id_t        AS id_t;
+CREATE DOMAIN comment_id_t      AS id_t;
+
 
 /************************************************************************/
 /* Timezones.								*/
